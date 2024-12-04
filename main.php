3cbaeaba741884ce9ada21197d2cebc98041c5fb
chmodddd <ribelcyberteam@gmail.com>
2024-12-04 22:20:01 +0800

    Bypass main.php
    detect filemanager

--- main.php
+++ main.php
@@ -4,9 +4,6 @@
 -->
 <!DOCTYPE html>
 <html lang="id">
-
-<!DOCTYPE html>
-<html lang="id">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">

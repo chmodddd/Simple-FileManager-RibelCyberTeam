ae405557296f94b13cd89d43f05c93033ea81573
RibelCyberTeam <ribelcyberteam@gmail.com>
2024-12-04 22:42:11 +0800

    Lihat main.php
    Bypass

--- main.php
+++ main.php
@@ -1,13 +1,12 @@
-<!--
-    Jangan diambil bangsaddd, minimal coding sendiri cokkkkkk.
-    yang ngubah mamaknya lonte, anak haram, gapunya akhlak, kontol yang ngubah pokoknya.
--->
 <!DOCTYPE html>
 <html lang="id">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="robots" content="noindex, nofollow">
+    <meta name="description" content="Simple FileManager By RibelCyberTeam">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="icon" href="https://backlinkku.id/images/icon/kurakura.png" type="image/png">
     <title>Simple FileManager By RibelCyberTeam</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 </head>
